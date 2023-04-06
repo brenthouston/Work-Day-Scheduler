3666221fa8936856824c67148d6fd6270ca48c1d
Brent Houston <brentjustinhouston@gmail.com>
2023-04-06 14:49:31 -0700

    text saved to storage and can print to text area

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 https://blog.logrocket.com/localstorage-javascript-complete-guide/
 
+https://blog.logrocket.com/storing-retrieving-javascript-objects-localstorage/
+
 
 https://day.js.org/docs/en/display/from-now
--- assets/script.js
+++ assets/script.js
@@ -3,24 +3,46 @@
 $(function () {
 
     var saveBtn = $('.btn');
-    var printTask = $('description')
-    
-    
-    
+    var taskBox9 = $('#task9')
+    var taskBox10 = $('#task10')
+    var taskBox11 = $('#task11')
+    var taskBox12 = $('#task12')
+    var taskBox13 = $('#task13')
+    var taskBox14 = $('#task14')
+    var taskBox15 = $('#task15')
+    var taskBox16 = $('#task16')
+    var taskBox17 = $('#task17')
     
     for(var i = 0 ;i < saveBtn.length; i++){
-        saveBtn[i].addEventListener("click",function(e){
-            e.preventDefault();
-            var inputVal = $(this).prev().val(); 
-            localStorage.setItem('input',inputVal);
-            
-            
+        saveBtn[i].addEventListener("click",function(){
+            var taskBox = {};
+            taskBox["inputVal"] = $(this).prev().val();
+            taskBox["sched-hour"] = $(this).parent().attr("id")
+            var taskBoxString = JSON.stringify(taskBox);
+            localStorage.setItem("taskBox",taskBoxString);
             ;})
+            
         };
+    
+    function renderTextContent (){
+        document.getElementById("task9").value = JSON.parse(localStorage.getItem("taskBox")).inputVal;
+    }
+
+    
+    renderTextContent();
         
-        
-        
-        
+    function PPF(){
+        setInterval(() => {
+        var hrTime = dayjs()
+        hrTime.format('HH');
+        console.log(hrTime)
+        },1000);    
+        if (taskBoxString < hrTime){
+            set
+        }
+   
+    };
+
         
         
         
