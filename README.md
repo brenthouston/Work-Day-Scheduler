5a52e759d2af25cc71ce195ca26187d0961705fd
Brent Houston <brentjustinhouston@gmail.com>
2023-04-06 00:27:02 -0700

    DayJS for working current time added

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Work-Day-Scheduler
 
 https://blog.logrocket.com/localstorage-javascript-complete-guide/
+
+
+https://day.js.org/docs/en/display/from-now
--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,41 @@
 
 
 $(function () {
-    
+
     var saveBtn = $('.btn');
+    var printTask = $('description')
+    
+    
+    
     
     for(var i = 0 ;i < saveBtn.length; i++){
-      saveBtn[i].addEventListener("click",function(){
-        var inputVal = $(this).prev().val(); 
-        localStorage.setItem('input',inputVal);
+        saveBtn[i].addEventListener("click",function(e){
+            e.preventDefault();
+            var inputVal = $(this).prev().val(); 
+            localStorage.setItem('input',inputVal);
+            
+            
+            ;})
+        };
         
-      ;})
-    };
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        //current time
+        setInterval(() =>{
+            var currentTime = dayjs();
+            $('#currentDay').text(currentTime.format('MMM D, YYYY, hh:mm:ss a'));
+            },1000);
+    
+
     });
