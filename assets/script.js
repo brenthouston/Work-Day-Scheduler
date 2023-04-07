041a5ea1e923be491a330e1e2e456244f25b2553
Brent Houston <brentjustinhouston@gmail.com>
2023-04-06 20:07:26 -0700

    set text area to dynamically respond to current hour

--- assets/script.js
+++ assets/script.js
@@ -1,63 +1,75 @@
-
-
 $(function () {
+  var saveBtn = $(".btn");
 
-    var saveBtn = $('.btn');
-    var taskBox9 = $('#task9')
-    var taskBox10 = $('#task10')
-    var taskBox11 = $('#task11')
-    var taskBox12 = $('#task12')
-    var taskBox13 = $('#task13')
-    var taskBox14 = $('#task14')
-    var taskBox15 = $('#task15')
-    var taskBox16 = $('#task16')
-    var taskBox17 = $('#task17')
-    
-    for(var i = 0 ;i < saveBtn.length; i++){
-        saveBtn[i].addEventListener("click",function(){
-            var taskBox = {};
-            taskBox["inputVal"] = $(this).prev().val();
-            taskBox["sched-hour"] = $(this).parent().attr("id")
-            var taskBoxString = JSON.stringify(taskBox);
-            localStorage.setItem("taskBox",taskBoxString);
-            ;})
-            
-        };
-    
-    function renderTextContent (){
-        document.getElementById("task9").value = JSON.parse(localStorage.getItem("taskBox")).inputVal;
-    }
+  for (var i = 0; i < saveBtn.length; i++) {
+    saveBtn[i].addEventListener("click", function () {
+      var taskBox = JSON.parse(localStorage.getItem("taskBox")) || {};
+    //   taskBox["inputVal"] = $(this).prev().val();
+    //   taskBox["sched-hour"] = $(this).parent().attr("id");
 
-    
-    renderTextContent();
-        
-    function PPF(){
-        setInterval(() => {
-        var hrTime = dayjs()
-        hrTime.format('HH');
-        console.log(hrTime)
-        },1000);    
-        if (taskBoxString < hrTime){
-            set
-        }
-   
-    };
+        // {
+        //     inputVal: "text",
+        //     sched-hour: 9
+        // }
 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        //current time
-        setInterval(() =>{
-            var currentTime = dayjs();
-            $('#currentDay').text(currentTime.format('MMM D, YYYY, hh:mm:ss a'));
-            },1000);
+        var id = $(this).parent().attr("id");
+        var text = $(this).prev().val();
+
+        taskBox["task"+id] = text;
+
+        // {
+        //     task9: "text",
+        //     task10: "hello"
+        // }
     
 
+      var taskBoxString = JSON.stringify(taskBox);
+      localStorage.setItem("taskBox", taskBoxString);
     });
+  }
+
+  function renderTextContent() {
+    var taskArray = $(".description");
+
+    for (var i = 0; i < taskArray.length; i++) {
+        // sets the text
+      $(taskArray[i]).val(JSON.parse(localStorage.getItem("taskBox"))["task"+(i+9)]);
+
+      // set the color
+      var targetDiv = $(taskArray[i]).parent();
+      var currentHour = parseInt(dayjs().format("H"));
+    // var currentHour = 16;
+      var currentId = parseInt(targetDiv.attr("id"))
+
+      console.log(currentHour)
+      console.log(targetDiv.attr("id"))
+
+      if(currentHour == currentId) {
+          targetDiv.addClass("present");
+      } else if(currentHour > currentId) {
+        targetDiv.addClass("past");
+      } else {
+        targetDiv.addClass("future");
+      }
+    }
+  }
+
+  renderTextContent();
+
+//   function PPF() {
+//     setInterval(() => {
+//       var hrTime = dayjs();
+//       hrTime.format("HH");
+//       console.log(hrTime);
+//     }, 1000);
+//     if (taskBoxString < hrTime) {
+//       set;
+//     }
+//   }
+
+  //current time
+  setInterval(() => {
+    var currentTime = dayjs();
+    $("#currentDay").text(currentTime.format("MMM D, YYYY, hh:mm:ss a"));
+  }, 1000);
+});
